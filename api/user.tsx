bae99b88dc485beeb9fb70450a8acbadafe46f17
alfredo.salzillo <noctisbels@gmai.com>
2020-11-15 21:52:19 +0100

    chore(badges user): fix typings

--- api/user.tsx
+++ api/user.tsx
@@ -1,7 +1,11 @@
 import { ServerRequest } from 'https://deno.land/std@0.77.0/http/server.ts';
-// @deno-types="https://deno.land/x/servest@v1.1.6/types/react/index.d.ts"
+// @ts-ignore
+import type from "https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts";
+// @ts-ignore
+import type from "https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/server.d.ts";
+// @deno-types="https://denopkg.com/soremwar/deno_types/react/v16.13.1/react.d.ts"
 import React from 'https://dev.jspm.io/react@16.13.1'
-// @deno-types="https://deno.land/x/servest@v1.1.6/types/react-dom/server/index.d.ts"
+// @deno-types="https://denopkg.com/soremwar/deno_types/react-dom/v16.13.1/server.d.ts"
 import ReactDOMServer from 'https://dev.jspm.io/react-dom@16.13.1/server';
 import { fetchUserByUsername, User } from '../lib/users.ts';
 
